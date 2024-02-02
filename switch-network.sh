766bb6984e776f921cc692d22cb9c08c077150e7
Hugh Mandalidis <hmandalidis@atlassian.com>
2024-02-02 17:17:12 +1100

    fix: iptables command update

--- switch-network.sh
+++ switch-network.sh
@@ -6,6 +6,7 @@
 # This should be run at least once sysctl -w net.ipv4.ip_forward=1
 #
 # Need OUTPUT for local machine
-iptables -t nat -A OUTPUT -p tcp --dport 8000 -j REDIRECT --to-port $1 
+iptables -t nat -D OUTPUT -p tcp --dport 8000 -j REDIRECT --to-port $1
+iptables -t nat -A OUTPUT -p tcp --dport 8000 -j REDIRECT --to-port $2 
 
 # iptables -t nat -A PREROUTING -p tcp --dport 8000 -j REDIRECT --to-port $1 won't work on local
