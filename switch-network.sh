084aae8b8d82c4d9b9fc07b08724db71d4be0c1a
Hugh Mandalidis <hmandalidis@atlassian.com>
2024-02-02 19:16:09 +1100

    chore: added sleep

--- switch-network.sh
+++ switch-network.sh
@@ -7,6 +7,7 @@
 #
 # Need OUTPUT for local machine
 iptables -t nat -D OUTPUT -p tcp --dport 8000 -j REDIRECT --to-port $1
+sleep 2
 iptables -t nat -A OUTPUT -p tcp --dport 8000 -j REDIRECT --to-port $2 
 
 # iptables -t nat -A PREROUTING -p tcp --dport 8000 -j REDIRECT --to-port $1 won't work on local
