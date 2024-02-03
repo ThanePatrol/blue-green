ffa3e5c67143f3b9c25263fe4f18e5369375ce6d
Hugh Mandalidis <hmandalidis@atlassian.com>
2024-02-03 12:37:06 +1100

    feat: start on rust controller for demo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    //start docker containers
+    std::process::Command::new("sh")
+        .arg("-c")
+        .arg("cd dummy-service && docker-compose up -d");
+
+    //set blue to be live first
+
     let blue = "http://localhost:8080";
     println!("Hello, world!");
 }
