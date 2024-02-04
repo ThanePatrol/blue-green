7147cc70f5178073dbf2a15c9bbdccfe62d5bd90
Hugh Mandalidis <hmandalidis@atlassian.com>
2024-02-04 20:57:21 +1100

    chore: remove comments

--- src/main.rs
+++ src/main.rs
@@ -36,40 +36,16 @@ impl CurrentDeploy {
     }
 }
 
-//   ports:
-//      - "8080:8080"
-//      - "8081:8081"
-//      - "8082:8082"
-//      - "9000:9999"
-//  ingress-green:
-//    image: service-proxy
-//    container_name: ingress-green
-//    ports:
-//      - "8010:8080"
-//      - "8011:8081"
-//      - "8012:8082"
-//      - "9010:9999"
-
 //NB - this requires to be run as root for the iptables commands to succeed
 fn main() {
-    //println!("Creating docker containers...");
-    ////start docker containers
-    //let output = std::process::Command::new("sh")
-    //    .arg("-c")
-    //    .arg("cd ~/service-proxy && make start-local")
-    //    .output()
-    //    .expect("error running docker - is it installed and docker daemon running?");
-
-    //println!("Done!");
     let iptables_error_string = "Error running iptables, are you root?";
 
     println!("Listening blue deploy on port 8000");
     if let Ok(mut current_deploy) = init() {
         let mut buffer = String::new();
 
-        println!("run curl http://localhost:8000 to see the current deploy");
+        println!("run curl http://localhost:8000/color to see the current deploy");
         println!("Enter one of the following strings: 'blue' 'green' or 'exit'");
-        // we read from stdin and change listener rules as appropriate
         loop {
             std::io::stdin()
                 .read_line(&mut buffer)
